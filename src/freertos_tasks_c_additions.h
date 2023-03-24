ce33ea19730eb51d1ade3fb7caa8cbd41dd2e01d
Andy Shaw <andy-git@gloomy-place.com>
2023-03-24 16:27:02 +0000

    Bring into line with Duet 3.5beta3 changes

--- src/freertos_tasks_c_additions.h
+++ src/freertos_tasks_c_additions.h
@@ -165,4 +165,9 @@ const StackType_t *pxTaskGetCurrentStackBase() noexcept
 	return pxCurrentTCB->pxStack;
 }
 
+// Return the top of stack at the last time the task was swapped out
+const volatile StackType_t *pxTaskGetLastStackTop(TaskHandle_t xTask) noexcept
+{
+	return xTask->pxTopOfStack;
+}
 // End
